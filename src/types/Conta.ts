0546c76267f65e4f5ce9e4167c1d3f941bf456be
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-05-31 11:36:26 -0300

    create validation decorator

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,4 +1,5 @@
 import { Armazenador } from "./Armazenador.js";
+import { ValidaDebito } from "./Decorators.js";
 import { GrupoTransacao } from "./GrupoTransacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 import { Transacao } from "./Transacao.js";
@@ -76,14 +77,8 @@ export class Conta {
     Armazenador.salvar("transacoes", JSON.stringify(this.transacoes));
   }
 
+  @ValidaDebito
   private debitar(valor: number): void {
-    if (valor <= 0) {
-      throw new Error("O valor a ser debitado deve ser maior que zero!");
-    }
-    if (valor > this.saldo) {
-      throw new Error("Saldo insuficiente!");
-    }
-
     this.saldo -= valor;
     Armazenador.salvar("saldo", this.saldo.toString());
   }
